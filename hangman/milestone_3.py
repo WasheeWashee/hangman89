1e759f0108ba911898b255c9fa0cfc04b77924de
WasheeWashee <67511533+WasheeWashee@users.noreply.github.com>
2023-11-13 23:46:34 +0000

    Update milestone_3.py
    Implemented checking whether guess was in the word

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -1,9 +1,20 @@
+import random
+
+word_list = ["Apple", "Banana", "Grapes", "Peaches", "Oranges"]
+print(word_list)
+
+word = random.choice(word_list)
+print(word)
+
 status = True
 guess = input("Please guess a letter: ")
 
 while status == True:
     if len(guess) == 1 and guess.isalpha():
-        print("Good guess!")
+        if guess.upper() or guess.lower() in word:
+           print(f"Good guess! {guess} is in the word.")
+        else:
+            print(f"Sorry, {guess} is not in the word. Try again.")
         status = False
     else:
         guess = input("Invalid letter. Please, enter a single alphabetical character. ")
