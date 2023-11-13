e63986672eb6e5872e7c0f5e1847886db212ea23
WasheeWashee <67511533+WasheeWashee@users.noreply.github.com>
2023-11-13 17:01:29 +0000

    Update milestone_2.py
    Added input validation

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -6,3 +6,10 @@ print(word_list)
 word = random.choice(word_list)
 
 print(word)
+
+guess = input("Please guess a letter: ")
+
+if len(guess) == 1 and guess.isalpha():
+    print("Good guess!")
+else:
+    print("Oops! That is not a valid input.")
