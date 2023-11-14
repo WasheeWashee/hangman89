229a92a2242d0f1394566466471c3ef941fde8f1
WasheeWashee <67511533+WasheeWashee@users.noreply.github.com>
2023-11-14 15:57:43 +0000

    Update milestone_3.py
    Defined check and ask functions

--- hangman/milestone_3.py
+++ hangman/milestone_3.py
@@ -6,15 +6,19 @@ print(word_list)
 word = random.choice(word_list)
 print(word)
 
-status = True
-guess = input("Please guess a letter: ")
-
-while status == True:
-    if len(guess) == 1 and guess.isalpha():
-        if guess.upper() or guess.lower() in word:
-           print(f"Good guess! {guess} is in the word.")
-        else:
-            print(f"Sorry, {guess} is not in the word. Try again.")
-        status = False
+def check_guess(guess):
+    if guess.upper() or guess.lower() in word:
+        print(f"Good guess! {guess} is in the word.")
     else:
-        guess = input("Invalid letter. Please, enter a single alphabetical character. ")
+        print(f"Sorry, {guess} is not in the word. Try again.")
+        
+
+def ask_for_input():
+    guess = input("Please guess a letter: ")
+    status = True
+    while status == True:
+        if len(guess) == 1 and guess.isalpha():
+            check_guess(guess)
+            status = False
+        else:
+            guess = input("Invalid letter. Please, enter a single alphabetical character: ")
